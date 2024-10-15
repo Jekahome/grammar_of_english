c3a8a5192be2e8d24ff994eb7bfef675490ca065
Jeka <>
2024-10-15 11:32:49 +0200

    what

--- practice.js
+++ practice.js
@@ -8,7 +8,7 @@ class Practice {
     #voice; 
     #recognizerResult = '';
     #callbackId;
-    callbackRecognition = null;
+    #callbackRecognition = null;
 
     static recognizer = null;
     static synth = null;
@@ -62,7 +62,8 @@ Practice.utterThis.lang = 'en-US';
         Practice.synth = window.speechSynthesis;
         //Practice.storeAudio = storeAudio;
         this.#voice = null;
-        this.callbackRecognition = callback;
+        this.#callbackRecognition = callback;
+        this.#callbackRecognition = this.#callbackRecognition.bind(this);
         //this.#play_success = document.getElementById("play_success");
         //this.#play_success.load();
         //this.#play_success.volume = 0.3;
@@ -134,7 +135,7 @@ Practice.utterThis.lang = 'en-US';
 
 
             console.log(`распознано: ${this.#recognizerResult}`)
-            this.callbackRecognition(this.#callbackId, this.#recognizerResult);
+            this.#callbackRecognition(this.#callbackId, this.#recognizerResult);
             // управление должно быть в методе инициировавшим распознание
             // if (Practice.speechStart == false){Practice.recognizer.start();}
             this.#recognizerResult='';
@@ -184,7 +185,7 @@ Practice.utterThis.lang = 'en-US';
             console.info('onaudioend закончил захват звука');
             if (this.#recognizerResult != ''){
                 console.log('----->',`${this.#recognizerResult}`);
-                this.callbackRecognition(this.#callbackId, this.#recognizerResult);
+                this.#callbackRecognition(this.#callbackId, this.#recognizerResult);
                 this.#recognizerResult='';
             }
             Practice.recognizer.stop();
