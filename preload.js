05e780ddf2d1fb5f43e028498c283813ac57b3a7
Jeka <>
2024-10-20 12:33:48 +0200

    what

--- preload.js
+++ preload.js
@@ -24,3 +24,61 @@ document.addEventListener('DOMContentLoaded', function () {
         });        
 });
 
+function checkAnswer(inputElement, correctAnswer) {
+    if (inputElement.value.length > 4){
+        inputElement.style.width = (inputElement.value.length + 1) + 'ch';
+    }
+
+    const contractions = {
+        "don't": "do not",
+        "can't": "cannot",
+        "won't": "will not",
+        "isn't": "is not",
+        "aren't": "are not",
+        "i'm": "i am",
+        "he's": "he is",
+        "she's": "she is",
+        "it's": "it is",
+        "we're": "we are",
+        "they're": "they are",
+        "i've": "i have",
+        "you've": "you have",
+        "we've": "we have",
+        "they've": "they have",
+        "i'll": "i will",
+        "you'll": "you will",
+        "he'll": "he will",
+        "she'll": "she will",
+        "it'll": "it will",
+        "we'll": "we will",
+        "they'll": "they will",
+        "i'd": "i would",
+        "you'd": "you would",
+        "he'd": "he would",
+        "she'd": "she would",
+        "it'd": "it would",
+        "we'd": "we would",
+        "they'd": "they would"
+    };
+
+    function replaceContractions(text) {
+        for (const contraction in contractions) {
+            const regex = new RegExp(`\\b${contraction}\\b`, 'gi');
+            text = text.replace(regex, contractions[contraction]);
+        }
+        return text;
+    }
+
+    let userAnswer = inputElement.value.trim().toLowerCase().replace(/[^a-zа-яё']/g, '');
+    userAnswer = replaceContractions(userAnswer);
+
+    let normalizedCorrectAnswer = correctAnswer.trim().toLowerCase().replace(/[^a-zа-яё']/g, '');
+    normalizedCorrectAnswer = replaceContractions(normalizedCorrectAnswer);
+
+    if (userAnswer === normalizedCorrectAnswer) {
+        inputElement.className = 'input-size correct'; 
+    } else {
+        inputElement.className = 'input-size incorrect'; 
+    }
+}
+
