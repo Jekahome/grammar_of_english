88ef67706badd835e6c1b8f5596e23537c2561f6
Jeka <>
2024-10-24 16:55:02 +0200

    what

--- preload.js
+++ preload.js
@@ -106,3 +106,12 @@ function showClue(id) {
 function removeParentheses(text) {
     return text.replace(/\(.*?\)/g, '').trim();
 }
+
+function shuffleSentence(sentence) {
+    const words = sentence.split(" ");
+    for (let i = words.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));   
+        [words[i], words[j]] = [words[j], words[i]];    
+    }
+    return words.join(" ");
+}
