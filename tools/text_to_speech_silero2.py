46b9e554618fe71f3b674a21db65cdda544089fe
Jeka <>
2024-11-27 17:47:33 +0100

    what

--- tools/text_to_speech_silero2.py
+++ tools/text_to_speech_silero2.py
@@ -10,6 +10,9 @@ https://habr.com/ru/articles/660565/
 https://github.com/snakers4/silero-models?tab=readme-ov-file
 https://models.silero.ai/models/tts/
 
+Голоса:
+https://oobabooga.github.io/silero-samples/
+
 Dependencies:
 sudo apt update && sudo apt install ffmpeg
 pip install numpy
@@ -64,7 +67,7 @@ def quality_improvement_audio(file_to):
 
 def combined():
     chunk_en = AudioSegment.from_file("sound/play_en.wav", format="wav")
-    chunk_en_slowly = AudioSegment.from_file("sound/play_en_slowly.wav", format="wav")
+    # chunk_en_slowly = AudioSegment.from_file("sound/play_en_slowly.wav", format="wav")
     chunk_ru = AudioSegment.from_file("sound/play_ru.wav", format="wav")
   
     chunk_pause_2 = AudioSegment.silent(duration=1000)
@@ -72,20 +75,20 @@ def combined():
 
     if os.path.exists('sound/play.wav') == False:
         # Склеивание аудиофайлов
-        combined_audio = chunk_en_slowly + chunk_pause_2 + chunk_en + chunk_pause_2 + chunk_ru + chunk_pause_1
-         
+        #combined_audio = chunk_en_slowly + chunk_pause_2 + chunk_en + chunk_pause_2 + chunk_ru + chunk_pause_1
+        combined_audio = chunk_ru + chunk_pause_1 + chunk_pause_2 + chunk_en + chunk_pause_2
         # Сохранение объединенного аудиофайла
         combined_audio.export("sound/play.wav", format="wav")
     else:
         full_play = AudioSegment.from_file("sound/play.wav", format="wav")
         # Склеивание аудиофайлов
-        combined_audio = full_play + chunk_en_slowly + chunk_pause_2 + chunk_en + chunk_pause_2 + chunk_ru + chunk_pause_1
+        combined_audio = full_play + chunk_ru + chunk_pause_1 + chunk_pause_2 + chunk_en + chunk_pause_2
           
         # Сохранение объединенного аудиофайла
         combined_audio.export("sound/play.wav", format="wav")
         
     os.remove("sound/play_en.wav")
-    os.remove("sound/play_en_slowly.wav")  
+    # os.remove("sound/play_en_slowly.wav")  
     os.remove("sound/play_ru.wav")   
  
 
@@ -98,35 +101,14 @@ def save_line_to_wav(line):
     text_en = text_ru = text_en_slowly = ''
     # для случая множества тире
     if "—" in line:
-        text = line.split('—',1)
+        text = line.split("—",1)
         if len(text) < 2:
             return 1
         text_en = text[0].rstrip()
         text_ru = text[1].lstrip()
         text_en_slowly = text_en
     else:
-        count_dash = line.count("-")
-        if  count_dash == 1:  
-            text = line.split('-',1)
-            if len(text) < 2:
-                return 1
-            text_en = text[0].rstrip()
-            text_ru = text[1].lstrip()
-            text_en_slowly = text_en
-        elif count_dash > 1:
-            index_dash = 0
-            for char in line:
-                index_dash +=1
-                if 'а' <= char <= 'я' or 'А' <= char <= 'Я':
-                    break;
-            if  index_dash == len(line):
-                return 1
-            text_en = line[:index_dash-1].rstrip()
-            text_en = text_en[:-1].rstrip()
-            text_ru = line[index_dash-1:].lstrip()
-            text_en_slowly = text_en
-        else:
-            return 1
+        return 1
 
     text_en = "<speak><prosody rate=\"medium\">" + text_en + "</prosody></speak>"
     text_en_slowly = ", ".join(text_en_slowly.split(' '))
@@ -138,11 +120,13 @@ def save_line_to_wav(line):
     )
     os.rename(output,'sound/play_en.wav')
 
+    '''
     output = model_en.save_wav(text=text_en_slowly,
         speaker=speaker_en,
         sample_rate=sample_rate,
     )
     os.rename(output,'sound/play_en_slowly.wav')
+    '''
 
     output = model_ru.save_wav(text=text_ru,
         speaker=speaker_ru,
